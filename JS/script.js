09e1ae2142a2e614de33430e6488dfa4e2227928
Hallexxx <112977841+Hallexxx@users.noreply.github.com>
2024-05-19 11:44:05 +0200

    u

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,5 @@
 window.onload = function () {
-
-    window.addEventListener('scroll', function () {
-        var rect = projectSection.getBoundingClientRect();
-
-        if (rect.top < window.innerHeight && rect.bottom > 0) {
-            // The "Projects" section is in view
-            enlargeElements();
-        } else {
-            resetElements();
-        }
-    });
-
+    // Ajoutez un écouteur d'événements pour la défilement de la fenêtre
     window.addEventListener("scroll", function() {
         var backToTopButton = document.querySelector(".back-to-top");
         if (window.pageYOffset > 300) {
@@ -20,15 +9,32 @@ window.onload = function () {
         }
     });
 
-    document.getElementById("myButton").addEventListener("click", function() {
-        // Trouvez l'élément de destination
-        var destination = document.getElementById("jeuu");
+    // Vérifiez si l'élément avec l'ID "myButton" existe
+    var myButton = document.getElementById("myButton");
+    if (myButton) {
+        // Ajoutez un écouteur d'événements au bouton
+        myButton.addEventListener("click", function() {
+            // Trouvez l'élément de destination
+            var destination = document.getElementById("jeuu");
 
-        // Faites défiler jusqu'à l'élément de destination
-        destination.scrollIntoView({ behavior: "smooth" });
+            // Faites défiler jusqu'à l'élément de destination
+            destination.scrollIntoView({ behavior: "smooth" });
 
-        // Affichez l'élément de destination avec une transition
-        destination.style.marginTop = "0";
-        destination.style.opacity = "1";
-    }); 
+            // Affichez l'élément de destination avec une transition
+            destination.style.marginTop = "0";
+            destination.style.opacity = "1";
+        });
+    }
+
+    const conteneur = document.getElementById('conteneur');
+    const texteCache = document.getElementById('texteCache');
+
+    conteneur.addEventListener('mouseover', () => {
+        texteCache.style.display = 'block';
+    });
+
+    conteneur.addEventListener('mouseout', () => {
+        texteCache.style.display = 'none';
+    });
+
 };
